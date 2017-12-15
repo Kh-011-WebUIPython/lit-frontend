def615793aa375fd98d24985f9e1956311ecb460
Olena Holub <else.golub@gmail.com>
2017-12-15 17:32:40 +0200

    Add section About Lit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import FirstScreen from './components/first-screen';
+import AboutLit from './components/about-lit'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <FirstScreen/>
+                <AboutLit />
             </div>
         );
     }
