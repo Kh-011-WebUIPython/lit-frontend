9b84698b0e868f6bd5c29b45e8f0208398a8c342
Olena Holub <else.golub@gmail.com>
2017-12-19 16:30:10 +0200

    Add search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 import logo from '../logo.svg';
+import {Form, FormGroup, Input} from 'reactstrap';
 
 class Search extends Component {
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
     render() {
         return (
-            <div className="search p-5">
-                <img src={logo} alt="LIT logo" className="logo-image mr-2"/>
-
+            <div className="search flex">
+                <img src={logo} alt="LIT logo" className="logo-image"/>
+                <Form className="ml-auto mr-auto">
+                    <FormGroup>
+                        <Input name="seacrh" type="text"
+                               placeholder="seacrh"
+                               onChange={this.handleInputChange}/>
+                    </FormGroup>
+                </Form>
             </div>
         );
     }
 }
 
 export default Search;
+
