8678c0947639bad982b1b27d8bab7bb466d618e5
Olena Holub <else.golub@gmail.com>
2017-12-19 11:48:49 +0200

    Add route for user page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import User from './User';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/reset.css';
 import './styles/base.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <main>
+            <Switch>
+                <Route exact path='/' component={App}/>
+                <Route path='/user' component={User}/>
+            </Switch>
+        </main>
+    </BrowserRouter>
+), document.getElementById('root'));
