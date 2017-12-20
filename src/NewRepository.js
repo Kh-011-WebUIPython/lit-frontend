8075369ddabd4548261bcf6ff7476501aac27ef0
Olena Holub <else.golub@gmail.com>
2017-12-20 12:40:47 +0200

    Add all components for page

--- src/NewRepository.js
+++ src/NewRepository.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import Search from './components/search'
+import ContentNewRepo from './components/content-new-repo';
+import UserComponent from './components/user-component';
+import {Button} from 'reactstrap';
 
 
 class NewRepository extends Component {
     render() {
         return (
             <div className="flex h-100">
-
+                <aside className="flex flex-column w-300 p-3 s-dark">
+                    <Search/>
+                    <UserComponent/>
+                    <Button color="primary">Create a new repo</Button>
+                </aside>
+                <ContentNewRepo/>
             </div>
         );
     }
