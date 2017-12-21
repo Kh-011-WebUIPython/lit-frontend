a98089722140eea5b56f9936fc982327b4b93e49
Olena Holub <else.golub@gmail.com>
2017-12-21 12:12:39 +0200

    Add page "Empty repository"

--- src/components/howto-start.js
+++ src/components/howto-start.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
-
-const Code = props => {
-    return (
-        <p><span className="bg-grey py-1 monospaced f-9">{props.children}</span></p>
-    );
-};
+import Code from './code'
 
 class HowBegin extends Component {
     render() {
@@ -13,15 +8,20 @@ class HowBegin extends Component {
                 <div className="container pb-4">
                     <h1 className="display-4 pb-2">How to start</h1>
                     <h2 className="display-5 pb-2">PyPI</h2>
-                    <p className="pb-3">To install LitVCS, run this command in your terminal:</p>
+                    <p className="pb-3">To install LitVCS, run this command in
+                        your terminal:</p>
                     <Code>$ pip install litvcs</Code>
-                    <p className="pt-3">This is the preferred method to install LitVCS, as it will always install the
+                    <p className="pt-3">This is the preferred method to install
+                        LitVCS, as it will always install the
                         most recent stable release.</p>
                     <h2 className="display-5 pb-2 pt-4">Source files</h2>
-                    <p className="pb-3">In case you downloaded or cloned the source code from GitHub or your own fork,
-                        you can run the following to install cameo for development:</p>
-                    <Code>$ git clone https://github.com/Kh-011-WebUIPython/lit-cli</Code><br/>
-                    <Code>$ cd lit-cli</Code><br/>
+                    <p className="pb-3">In case you downloaded or cloned the
+                        source code from GitHub or your own fork,
+                        you can run the following to install cameo for
+                        development:</p>
+                    <Code>$ git clone
+                        https://github.com/Kh-011-WebUIPython/lit-cli</Code>
+                    <Code>$ cd lit-cli</Code>
                     <Code>$ pip install --editable .</Code>
                 </div>
             </section>
