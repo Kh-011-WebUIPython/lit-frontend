2f73c515604a78e7d31dba1d5abc30a09317c404
Olena Holub <else.golub@gmail.com>
2017-12-27 10:28:32 +0200

    Write components in different files

--- src/components/repo-form.js
+++ src/components/repo-form.js
@@ -2,11 +2,8 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import NavBarRepo from "./navbar-repo";
 import ListFiles from "./list-files";
-import {
-    Button, ButtonDropdown, DropdownItem, DropdownMenu,
-    DropdownToggle
-} from 'reactstrap';
-
+import BranchDropdown from './branch-dropdown';
+import {Button} from 'reactstrap';
 
 class RepositoryForm extends Component {
 
@@ -18,51 +15,18 @@ class RepositoryForm extends Component {
 
             <div>
                 <NavBarRepo/>
+                <div>
                 <Link to='/repository'><h2 className="py-4">RepoName</h2></Link>
                 <div className="border-dark">
+                    <span>Current branch:</span>
                     <BranchDropdown/>
                     <Link to='/repository'><Button className="ml-5"
                                                    color="primary">Pull
                         request</Button></Link>
                     <ListFiles/>
                 </div>
+                </div>
             </div>
-        );
-    }
-}
-
-
-class BranchDropdown extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            dropdownOpen: false
-        };
-    }
-
-    toggle() {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
-    }
-
-    render() {
-        return (
-            <ButtonDropdown isOpen={this.state.dropdownOpen}
-                            toggle={this.toggle}>
-                <DropdownToggle caret>
-                    Master
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem><Link
-                        to='/repository'>Developer</Link></DropdownItem>
-                    <DropdownItem><Link
-                        to='/repository'>Feature/Task_4/Something_do_it</Link></DropdownItem>
-                </DropdownMenu>
-            </ButtonDropdown>
         );
     }
 }
