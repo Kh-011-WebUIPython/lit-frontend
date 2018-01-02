5f5a2247ec616fddf74c8512483be533bf962603
Daria <kattdaria@gmail.com>
2018-01-02 22:20:53 +0200

    Fix react warning about link inside link.

--- src/HomePage/lit-logo.js
+++ src/HomePage/lit-logo.js
@@ -6,10 +6,8 @@ import {Link} from 'react-router-dom';
 const LitLogo = (props) => {
     return (
         <NavbarBrand className="logo">
-            <Link to='/'><img src={logo} alt="LIT logo"
-                              className="logo-image mr-2"/></Link>
-            <Link to='/'><h1 className="text-muted text-cursive">LIT</h1>
-            </Link>
+            <img src={logo} alt="LIT logo" className="logo-image mr-2"/>
+            <h1 className="text-muted text-cursive">LIT</h1>
         </NavbarBrand>
     );
 }
