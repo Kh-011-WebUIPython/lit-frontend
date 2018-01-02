4d322b189e074ffe3e065d482cd126368474df8d
Daria <kattdaria@gmail.com>
2018-01-03 01:12:15 +0200

    Add alert actions

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -4,13 +4,13 @@ import { reducer as formReducer } from 'redux-form'
 import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 // import { users } from './users.reducer';
-// import { alert } from './alert.reducer';
+import { alert } from './alert.reducer';
 
 const rootReducer = combineReducers({
     authentication,
     registration,
     // users,
-    // alert
+    alert,
     form: formReducer
 });
 
