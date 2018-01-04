6b98eb96e92284edaa2d9a1bf6084966cf4f40f2
Olena Holub <else.golub@gmail.com>
2018-01-04 23:15:39 +0200

    Add button dropdown for branches. Add textarea for description pull request.

--- src/_components/open-pull-request-form.js
+++ src/_components/open-pull-request-form.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 import NavBarRepo from './navbar-repo';
-import ListFiles from './list-files';
 import BranchDropdown from './branch-dropdown';
-import {Button} from 'reactstrap';
 import RepositoryTeam from './repo-team';
+import handshake from '../_img/handshake2.svg'
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class OpenPullRequestForm extends Component {
 
@@ -19,7 +19,31 @@ class OpenPullRequestForm extends Component {
                 <div>
                     <RepositoryTeam/>
                     <div className="border-dark">
-
+                        <div className="flex justify-content-around">
+                            <div
+                                className="flex flex-column align-items-center justify-content-center">
+                                <span className="mb-2">First branch:</span>
+                                <BranchDropdown/>
+                            </div>
+                            <img src={handshake} alt="handshake"
+                                 className="handshake-4"/>
+                            <div
+                                className="flex flex-column align-items-center justify-content-center">
+                                <span className="mb-2">Second branch:</span>
+                                <BranchDropdown/>
+                            </div>
+                        </div>
+                        <Form>
+                            <FormGroup>
+                                <Label className="h5 py-4">Write a description for pull
+                                    request</Label>
+                                <Input name="description" type="textarea"
+                                       onChange={this.handleInputChange}/>
+                            </FormGroup>
+                            <Link to='/open_pull_request'><Button
+                                color="primary">Open pull
+                                request</Button></Link>
+                        </Form>
                     </div>
                 </div>
             </div>
