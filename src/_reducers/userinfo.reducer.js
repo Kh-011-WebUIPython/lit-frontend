750c64daa1d4300f4a49a62fd826d4b3b0fe73c2
Daria <kattdaria@gmail.com>
2018-01-05 17:18:59 +0200

    Fetch data about user. Use mock for failure

--- src/_reducers/userinfo.reducer.js
+++ src/_reducers/userinfo.reducer.js
@@ -1,4 +1,4 @@
-import {userpageConstants} from '../_constants';
+import {userConstants, userpageConstants} from '../_constants';
 
 
 export function userinfo(state = {}, action) {
@@ -6,8 +6,16 @@ export function userinfo(state = {}, action) {
         case userpageConstants.USERINFO_REQUEST:
             return {fetchingUserinfo: true};
         case userpageConstants.USERINFO_SUCCESS:
-            return {userinfo: action.userinfo};
+            return {...action.userinfo};
         case userpageConstants.USERINFO_FAILURE:
+            //todo: remove this mock
+            return {
+                "pk": 1,
+                "username": "exceedcat",
+                "email": "exceed@cat.com",
+                "avatar": "https://78.media.tumblr.com/1f353e42115d7e62e80b35e9693816cc/tumblr_ot5b6yPohy1rugt9lo1_250.gif",
+            };
+        case userConstants.SIGNOUT:
             return {};
         default:
             return state
