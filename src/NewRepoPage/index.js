7a3dcf91112fb1de31c2941151ebee036e8b791b
Daria <kattdaria@gmail.com>
2018-01-10 01:18:22 +0200

    Refactor NewRepoPage

--- src/NewRepoPage/index.js
+++ src/NewRepoPage/index.js
@@ -2,21 +2,10 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Redirect} from 'react-router-dom';
 import CreateRepoForm from './form';
-import UserInfoBlock from '../UserInfoBlock';
-import {userActions, userpageActions} from "../_actions";
-import LoadingPage from '../_components/loading-page';
 
 class NewRepoPage extends Component {
-    componentDidMount() {
-        this.props.getUserInfo();
-    };
-
     render() {
-        if (this.props.userinfo.fetchingUserinfo) {
-            return (<LoadingPage/>)
-        }
-        const {avatar, username} = this.props.userinfo;
-        const {alert, signOut} = this.props;
+        const {alert, username} = this.props;
         const {repo} = this.props.repoCreation;
 
         if (repo) {
@@ -24,33 +13,17 @@ class NewRepoPage extends Component {
         }
 
         return (
-            <div className="flex h-100">
-                <UserInfoBlock avatar={avatar} username={username} signOut={signOut}/>
-                <div className="container pt-5 w-100">
-                    <CreateRepoForm alert={alert}/>
-                </div>
-            </div>
+            <CreateRepoForm alert={alert}/>
         );
     };
 }
 
 const mapStateToProps = state => {
     return {
-        userinfo: state.userinfo, alert: state.alert, repoCreation: state.repoCreation
+        username: state.userinfo.username, alert: state.alert, repoCreation: state.repoCreation
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        signOut: () => {
-            dispatch(userActions.signOut())
-        },
-        getUserInfo: () => {
-            dispatch(userpageActions.getUserInfo());
-        }
-    }
-};
-
-const ConnectedNewRepoPage = connect(mapStateToProps, mapDispatchToProps)(NewRepoPage);
+const ConnectedNewRepoPage = connect(mapStateToProps)(NewRepoPage);
 
 export default ConnectedNewRepoPage;
