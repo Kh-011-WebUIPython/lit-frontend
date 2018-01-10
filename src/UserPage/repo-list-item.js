c705285b4330a77993a83f2b86db96d9114bcc31
Daria <kattdaria@gmail.com>
2018-01-10 17:36:09 +0200

    Connect RepoListItem to store, to get username into link to repos

--- src/UserPage/repo-list-item.js
+++ src/UserPage/repo-list-item.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Author from './author';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-const RepoListItem = ({name, author, description}) => {
+const RepoListItem = ({name, user, author, description}) => {
     return (
         <div className="border p-2 pl-3 pb-3 mt-2">
             <div className="flex justify-content-between">
-                <h2><Link to={`/${name}`}>{name}</Link></h2>
+                <h2><Link to={`/${author || user}/${name}`}>{name}</Link></h2>
                 {author && <Author>{author}</Author>}
             </div>
             <p>{description}</p>
@@ -14,4 +15,8 @@ const RepoListItem = ({name, author, description}) => {
     );
 };
 
-export default RepoListItem;
+const mapStateToProps = state => {
+    return ({user: state.userinfo.username});
+}
+
+export default connect(mapStateToProps)(RepoListItem);
