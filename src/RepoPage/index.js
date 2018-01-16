35719f8f6ea49b14434c501dc6e7c61b719f0e34
Daria <kattdaria@gmail.com>
2018-01-16 20:59:36 +0200

    Refactor RepoPage

--- src/RepoPage/index.js
+++ src/RepoPage/index.js
@@ -1,29 +1,28 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import NavBarRepo from './navbar-repo';
 import ListFiles from './list-files';
 import BranchDropdown from './branch-dropdown';
-import {Button} from 'reactstrap';
 
-class RepositoryForm extends Component {
-    render() {
-        const repoName = this.props.location.pathname.split('/')[2];
-        return (
-            <div>
-                <NavBarRepo/>
-                <div>
-                    <Link to='/repository'><h2 className="pb-4">{repoName}</h2></Link>
-                    <div className="border-dark">
-                        <span className="mr-2">Current branch:</span>
-                        <BranchDropdown/>
-                        <Link to='/repository'><Button className="ml-5" color="primary">Pull request</Button></Link>
-                        <ListFiles/>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
+const RepositoryForm = (props) => {
+  const repoName = props.location.pathname.split('/')[2];
+  return (
+    <div>
+      <NavBarRepo />
+      <div>
+        <Link to="/repository"><h2 className="pb-4">{ repoName }</h2></Link>
+        <div className="border-dark">
+          <span className="mr-2">Current branch:</span>
+          <BranchDropdown />
+          <Link to="/repository">
+            <Button className="ml-5" color="primary">Pull request</Button>
+          </Link>
+          <ListFiles />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RepositoryForm;
