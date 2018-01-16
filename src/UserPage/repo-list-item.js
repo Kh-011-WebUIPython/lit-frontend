3f3e81ba79686e33952c6f468af3f884f63d1bed
Daria <kattdaria@gmail.com>
2018-01-16 21:16:14 +0200

    Refactor UserPage

--- src/UserPage/repo-list-item.js
+++ src/UserPage/repo-list-item.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Author from './author';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Author from './author';
 
 const RepoListItem = ({
   name, user, author, description,
 }) => (
   <div className="border p-2 pl-3 pb-3 mt-2">
     <div className="flex justify-content-between">
-      <h2><Link to={`/${author || user}/${name}`}>{name}</Link></h2>
-      {author && <Author>{author}</Author>}
+      <h2><Link to={`/${author || user}/${name}`}>{ name }</Link></h2>
+      { author && <Author>{ author }</Author> }
     </div>
-    <p>{description}</p>
+    <p>{ description }</p>
   </div>
 );
 
