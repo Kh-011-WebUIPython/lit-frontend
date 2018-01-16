843531f526fef7fbfb1aeb2f74113e214b37b504
Daria <kattdaria@gmail.com>
2018-01-16 20:44:24 +0200

    Refactor NewRepoPage

--- src/NewRepoPage/index.js
+++ src/NewRepoPage/index.js
@@ -1,36 +1,30 @@
-import React, {Component} from 'react';
-import {connect} from "react-redux";
-import {Redirect} from 'react-router-dom';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import CreateRepoForm from './form';
-import {repoActions} from "../_actions";
+import { repoActions } from '../_actions';
 
-class NewRepoPage extends Component {
-    render() {
-        const {alert, username} = this.props;
-        const {repo} = this.props.repoCreation;
+const NewRepoPage = (props) => {
+  const { alert, username } = props;
+  const { repo } = props.repoCreation;
 
-        if (repo) {
-            this.props.clear();
-            return <Redirect to={`/${username}/${repo.name}/empty`} push={true}/>
-        }
+  if (repo) {
+    this.props.clear();
+    return <Redirect to={ `/${username}/${repo.name}/empty` } push/>;
+  }
 
-        return (
-            <CreateRepoForm alert={alert}/>
-        );
-    };
-}
-
-const mapStateToProps = state => {
-    return {
-        username: state.userinfo.username, alert: state.alert, repoCreation: state.repoCreation
-    };
+  return (
+    <CreateRepoForm alert={ alert }/>
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-    return ({
-        clear: () => dispatch(repoActions.clearCreation()),
-    });
-}
+const mapStateToProps = state => ({
+  username: state.userinfo.username, alert: state.alert, repoCreation: state.repoCreation,
+});
+
+const mapDispatchToProps = dispatch => ({
+  clear: () => dispatch(repoActions.clearCreation()),
+});
 
 const ConnectedNewRepoPage = connect(mapStateToProps, mapDispatchToProps)(NewRepoPage);
 
