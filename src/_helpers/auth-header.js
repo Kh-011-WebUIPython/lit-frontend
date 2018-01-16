dc60b8347209f8829bc56104a2705465bb092232
Daria <kattdaria@gmail.com>
2018-01-16 20:24:29 +0200

    Refactor helpers

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,10 +1,9 @@
 export function authHeader() {
-    // return authorization header with token key
-    let user = JSON.parse(localStorage.getItem('user'));
+  // return authorization header with token key
+  const user = JSON.parse(localStorage.getItem('user'));
 
-    if (user && user.key) {
-        return {authorization: 'Token ' + user.key};
-    } else {
-        return {};
-    }
+  if (user && user.key) {
+    return { authorization: `Token ${user.key}` };
+  }
+  return {};
 }
--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,14 +1,14 @@
-import {applyMiddleware, createStore} from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import {createLogger} from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
 
 const loggerMiddleware = createLogger();
 
 export const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+  ),
 );
