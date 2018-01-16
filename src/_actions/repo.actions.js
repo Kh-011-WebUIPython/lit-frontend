b04128396c2fcfab50f264b94425880cceb45c10
Daria <kattdaria@gmail.com>
2018-01-16 20:01:23 +0200

    Refactor actions

--- src/_actions/repo.actions.js
+++ src/_actions/repo.actions.js
@@ -1,89 +1,92 @@
-import {repoConstants} from '../_constants';
-import {repoService} from '../_services';
-import {alertActions} from '.';
+import { repoConstants } from '../_constants';
+import { repoService } from '../_services';
+import { alertActions } from '.';
 
 
 export const repoActions = {
-    create,
-    clearCreation,
-    getByUser,
+  create,
+  clearCreation,
+  getByUser,
 };
 
 
-function create({name, description}) {
-    return dispatch => {
-        dispatch(request({name}));
+function create({ name, description }) {
+  return (dispatch) => {
+    dispatch(request({ name }));
 
-        repoService.create(name, description)
-            .then(
-                repo => {
-                    console.log(repo);
-                    dispatch(success(repo));
-                },
-                error => {
-                    dispatch(failure(error));
-                    dispatch(alertActions.error(error));
-                }
-            );
-    };
+    repoService.create(name, description)
+      .then(
+        (repo) => {
+          dispatch(success(repo));
+        },
+        (error) => {
+          dispatch(failure(error));
+          dispatch(alertActions.error(error));
+        },
+      );
+  };
 
-    function request(repo) {
-        return {type: repoConstants.CREATION_REQUEST, repo}
-    }
+  function request(repo) {
+    return { type: repoConstants.CREATION_REQUEST, repo };
+  }
 
-    function success(repo) {
-        return {type: repoConstants.CREATION_SUCCESS, repo}
-    }
+  function success(repo) {
+    return { type: repoConstants.CREATION_SUCCESS, repo };
+  }
 
-    function failure(error) {
-        return {type: repoConstants.CREATION_FAILURE, error}
-    }
+  function failure(error) {
+    return { type: repoConstants.CREATION_FAILURE, error };
+  }
 }
 
 function clearCreation() {
-    return {type: repoConstants.CREATION_CLEAR}
+  return { type: repoConstants.CREATION_CLEAR };
 }
 
 function getByUser(id) {
-    return async dispatch => {
-        try {
-            dispatch(request());
+  return async (dispatch) => {
+    try {
+      dispatch(request());
 
-            const repos = await repoService.getByUser(id)
-            const normalizedRepos = await handleRepos(repos).then(x => x);
+      const repos = await repoService.getByUser(id);
+      const normalizedRepos = await handleRepos(repos).then(x => x);
 
-            const reallyNormalized = normalizedRepos.owner.map(r => ({name: r.name, description: r.description}));
-            dispatch(success({owner: reallyNormalized, contributor: []}));
-        } catch (error) {
-            dispatch(failure(error));
-            dispatch(alertActions.error(error));
-        }
-    };
-
-    function request() {
-        return {type: repoConstants.GET_BY_USER_REQUEST}
+      const reallyNormalized = normalizedRepos.owner.map(r => ({
+        name: r.name,
+        description: r.description,
+      }));
+      dispatch(success({ owner: reallyNormalized, contributor: [] }));
+    } catch (error) {
+      dispatch(failure(error));
+      dispatch(alertActions.error(error));
     }
+  };
 
-    function success(repos) {
-        return {type: repoConstants.GET_BY_USER_SUCCESS, repos}
-    }
+  function request() {
+    return { type: repoConstants.GET_BY_USER_REQUEST };
+  }
 
-    function failure(error) {
-        return {type: repoConstants.GET_BY_USER_FAILURE, error}
-    }
+  function success(repos) {
+    return { type: repoConstants.GET_BY_USER_SUCCESS, repos };
+  }
 
-    async function handleRepos(repos) {
-        const ownerIds = repos.results.filter(user => user.status === 'owner').map(repo => repo.repository_id);
-        // const contributorIds = repos.results.filter(user => user.status !== 'owner').map(repo => repo.repository_id);
+  function failure(error) {
+    return { type: repoConstants.GET_BY_USER_FAILURE, error };
+  }
 
-        const oPromises = ownerIds.map(getRepoById);
+  async function handleRepos(repos) {
+    const ownerIds = repos.results.filter(user => user.status === 'owner').map(repo => repo.repository_id);
+    // const contributorIds = repos.results.filter(user => user.status !== 'owner').map(repo =>
+    // repo.repository_id);
 
-        // const cPromises = contributorIds.map(getRepoById);
-        const owner = await Promise.all(oPromises);
-        return ({owner: owner});
-    }
+    const oPromises = ownerIds.map(getRepoById);
 
-    async function getRepoById(id) {
-        return await repoService.getById(id);
-    }
+    // const cPromises = contributorIds.map(getRepoById);
+    const owner = await Promise.all(oPromises);
+    return ({ owner });
+  }
+
+  async function getRepoById(userId) {
+    return await repoService.getById(userId);
+  }
 }
