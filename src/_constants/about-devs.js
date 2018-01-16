e63f3e1de3353fd5f2cd424c02914ebe2e40a3c5
Daria <kattdaria@gmail.com>
2018-01-16 20:23:51 +0200

    Refactor constants

--- src/_constants/about-devs.js
+++ src/_constants/about-devs.js
@@ -1,46 +1,32 @@
-const devs = [
-    {
-        name: 'Dima Volodin',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-        avatar: 'https://picsum.photos/150/150',
-        bgimg: 'https://picsum.photos/300/180',
-    },
-    {
-        name: 'Max Krivich',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-        avatar: 'https://picsum.photos/150/150',
-        bgimg: 'https://picsum.photos/300/180',
-    },
-    {
-        name: 'Pavel Belousov',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-        avatar: 'https://picsum.photos/150/150',
-        bgimg: 'https://picsum.photos/300/180',
-    },
-    {
-        name: 'Lena Holub',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-        avatar: 'https://picsum.photos/150/150',
-        bgimg: 'https://picsum.photos/300/180',
-    },
-    {
-        name: 'Daria Pronina',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-        avatar: 'https://picsum.photos/150/150',
-        bgimg: 'https://picsum.photos/300/180',
-    },
-    // {
-    //     name: 'Name Surname',
-    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-    //     avatar: 'https://picsum.photos/150/150',
-    //     bgimg: 'https://picsum.photos/300/180',
-    // },
-    // {
-    //     name: 'Name Surname',
-    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
-    //     avatar: 'https://picsum.photos/150/150',
-    //     bgimg: 'https://picsum.photos/300/180',
-    // },
+export const devs = [
+  {
+    name: 'Dima Volodin',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
+    avatar: 'https://picsum.photos/150/150',
+    bgimg: 'https://picsum.photos/300/180',
+  },
+  {
+    name: 'Max Krivich',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
+    avatar: 'https://picsum.photos/150/150',
+    bgimg: 'https://picsum.photos/300/180',
+  },
+  {
+    name: 'Pavel Belousov',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
+    avatar: 'https://picsum.photos/150/150',
+    bgimg: 'https://picsum.photos/300/180',
+  },
+  {
+    name: 'Lena Holub',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
+    avatar: 'https://picsum.photos/150/150',
+    bgimg: 'https://picsum.photos/300/180',
+  },
+  {
+    name: 'Daria Pronina',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, sit!',
+    avatar: 'https://picsum.photos/150/150',
+    bgimg: 'https://picsum.photos/300/180',
+  },
 ];
-
-export default devs;
