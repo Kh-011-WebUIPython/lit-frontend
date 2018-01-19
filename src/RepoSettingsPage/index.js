34ca600b73edeb1515966e7f3e8dc86352b26835
Daria <kattdaria@gmail.com>
2018-01-19 18:07:49 +0200

    Add repo settings page

---
+++ src/RepoSettingsPage/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import RepositorySettingsForm from './form';
+import { getIdByName } from '../_helpers';
+import { repoActions } from '../_actions';
+
+const RepoSettings = (props) => {
+  const { repo } = props.repoUpdate;
+
+  // redirect if repo has already been successfully updated
+  if (repo) {
+    props.clear();
+    // 0 is empty and 3 is 'settings', we don't need it
+    const repoPage = props.location.pathname.split('/')
+      .reduce((res, cur, ind) => (ind === 0 || ind === 3 ? res : `${res}/${cur}`), '');
+
+    return <Redirect to={repoPage} push />;
+  }
+
+  const repoName = props.location.pathname.split('/')[2];
+  const repoId = getIdByName(repoName, props.repos);
+
+  // todo: add breadcrumbs
+  // todo: add breadcrumbs when you have created a repo
+  // todo: check if it exists before rendering
+  // todo: add confirm before user deletion
+  return (
+    <div>
+      <div className="flex justify-content-between align-items-baseline">
+        {/*<h2 className="pb-4">*/}
+          {/*<Link to={ownerLink}>{`${this.state.username} `}</Link>*/}
+          {/*/ {this.state.name}*/}
+        {/*</h2>*/}
+        <h2 className="pb-2">Update a repository</h2>
+        <span>Settings</span>
+      </div>
+      <RepositorySettingsForm id={repoId} name={repoName} />
+    </div>
+  );
+};
+
+
+const mapStateToProps = state => ({ repos: state.repos, repoUpdate: state.repoUpdate });
+
+const mapDispatchToProps = dispatch => ({
+  clear: () => dispatch(repoActions.clearUpdate()),
+});
+
+const ConnectedRepoSettings = connect(mapStateToProps, mapDispatchToProps)(RepoSettings);
+
+export default ConnectedRepoSettings;
