615fbc3fe973e8e4f4d7ea24d90d62bf69e7c167
Daria <kattdaria@gmail.com>
2018-01-22 22:04:27 +0200

    Refactor reducers

--- src/_reducers/user-repos.reducer.js
+++ src/_reducers/user-repos.reducer.js
@@ -3,7 +3,7 @@ import { repoConstants } from '../_constants';
 export function userRepos(state = {}, action) {
   switch (action.type) {
     case repoConstants.GET_BY_USER_REQUEST:
-      return { fetchingRepos: true };
+      return { fetching: true };
     case repoConstants.GET_BY_USER_SUCCESS:
       return { ...action.userRepos };
     case repoConstants.GET_BY_USER_FAILURE:
