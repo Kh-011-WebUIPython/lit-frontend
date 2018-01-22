f6f90a4310d2af004d2156c2c4458c9c8a94b3af
Daria <kattdaria@gmail.com>
2018-01-22 21:25:49 +0200

    Refactor alert

--- src/_reducers/alert.reducer.js
+++ src/_reducers/alert.reducer.js
@@ -2,11 +2,6 @@ import { alertConstants } from '../_constants';
 
 export function alert(state = {}, action) {
   switch (action.type) {
-    case alertConstants.SUCCESS:
-      return {
-        type: 'alert-success',
-        message: action.message,
-      };
     case alertConstants.ERROR:
       return {
         type: 'alert-danger',
